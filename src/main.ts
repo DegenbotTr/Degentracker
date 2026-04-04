3082c3ad3cebfdf472cb3c76fb122d8c25833f92
Olowodarey <olowodarey8@gmail.com>
2026-04-04 15:36:44 +0100

    build

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { execSync } from 'child_process';
 
 async function bootstrap() {
+  console.log('Running database migrations...');
+  execSync('npx prisma migrate deploy', { stdio: 'inherit' });
+
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
   console.log('Sol Wallet Watcher is running');
